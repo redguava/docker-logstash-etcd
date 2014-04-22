e573f771fdbebcc9dafbc7346004eab2557c51c0
Matthew Jones <matt@geekle.id.au>
2014-04-22 13:01:10 +1000

    Fix path, agin.

--- start-elasticsearch.sh
+++ start-elasticsearch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk.x86_64"
+export JAVA_HOME="/usr/lib/jvm/jre-1.7.0-openjdk.x86_64"
 export JAVACMD="${JAVA_HOME}/bin/java"
 /elasticsearch-1.1.0/bin/elasticsearch
